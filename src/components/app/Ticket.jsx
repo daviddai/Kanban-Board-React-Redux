edbc927481d42cee341f817bab895314dd18ec4c
ddai <ddai@tripadvisor.com>
2018-09-19 15:50:46 +1000

    implemented expandable ticket on kanban board

--- src/components/app/Ticket.jsx
+++ src/components/app/Ticket.jsx
@@ -7,6 +7,9 @@ import {CardFooter} from "../reusable/card/CardFooter";
 import TaskList from "./TaskList";
 import marked from "marked";
 
+import "bootstrap/dist/js/bootstrap.min";
+import "jquery/dist/jquery.min";
+
 class Ticket extends Component {
 
     constructor(props) {
@@ -40,7 +43,9 @@ class Ticket extends Component {
     );
 
     getTicketDetails = (ticketId) => (
-        <React.Fragment>
+        <div id={'ticket-detail-' + ticketId}
+             className="collapse show"
+        >
             <CardBody className="border-top-0 py-0">
                 <div className="text-left">
                     <p dangerouslySetInnerHTML={{__html:marked(this.props.description)}} />
@@ -52,31 +57,33 @@ class Ticket extends Component {
                     {this.getTaskInputBox(ticketId)}
                 </div>
             </CardFooter>
-        </React.Fragment>
+        </div>
     );
 
     render() {
-        let ticketDetails;
-
         const ticketLeftSideStyle = {
             borderLeft: this.props.color + " solid thick"
         };
 
-        if (this.state.showDetails) {
-            ticketDetails = this.getTicketDetails(this.props.id);
-        }
-
         return (
-            <Card className="mb-2 rounded-0 border-top-0 border-right-0 border-bottom-0" style={ticketLeftSideStyle}>
+            <Card id={'ticket-' + this.props.id}
+                  className="mb-2 rounded-0 border-top-0 border-right-0 border-bottom-0"
+                  aria-expanded="true"
+                  aria-controls={'ticket-detail-' + this.props.id}
+                  style={ticketLeftSideStyle}
+            >
                 <CardHeader className="border-bottom-0 ticket-color">
                     <h6 className="pull-left">
                         <i className={`fa ${this.state.showDetails ? 'fa-angle-down' : 'fa-angle-right'} pr-2 font-weight-bold`}
                            onClick={this.toggleTicketDetails}
+                           data-toggle="collapse"
+                           data-target={'#ticket-detail-' + this.props.id}
+                           aria-expanded="true" aria-controls={'ticket-detail-' + this.props.id}
                         />
                         {this.props.title}
                     </h6>
                 </CardHeader>
-                {ticketDetails}
+                {this.getTicketDetails(this.props.id)}
             </Card>
         );
     }
