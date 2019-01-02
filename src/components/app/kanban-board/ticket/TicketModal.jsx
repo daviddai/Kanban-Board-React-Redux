77d78d7e91000415c6f3414db679808734bce21e
ddai <ddai@tripadvisor.com>
2019-01-02 17:30:43 +1100

    create ticket form wip

--- src/components/app/kanban-board/ticket/TicketModal.jsx
+++ src/components/app/kanban-board/ticket/TicketModal.jsx
@@ -6,6 +6,7 @@ import Modal from "react-responsive-modal";
 import {Card} from "../../../reusable/card/Card";
 import {CardHeader} from "../../../reusable/card/CardHeader";
 import {CardBody} from "../../../reusable/card/CardBody";
+import {CardFooter} from "../../../reusable/card/CardFooter";
 
 export default class TicketModal extends Component {
 
@@ -29,7 +30,7 @@ export default class TicketModal extends Component {
         return (
             <Modal open={shouldOpen}
                    onClose={this.closeModal}
-                   styles="create-ticket-modal"
+                   classNames={{modal: "create-ticket-modal"}}
                    showCloseIcon={false}
             >
                 <form>
@@ -39,13 +40,40 @@ export default class TicketModal extends Component {
                         </CardHeader>
                         <CardBody>
                             <div className="form-group row">
-                                <label htmlFor="example-text-input" className="col-2 col-form-label">Text</label>
+                                <label className="col-2 col-form-label">Title:</label>
                                 <div className="col-10">
-                                    <input className="form-control" type="text" value="Artisanal kale"
-                                           id="example-text-input" />
+                                    <input className="form-control"
+                                           type="text"
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col-2 col-form-label">Description:</label>
+                                <div className="col-10">
+                                    <textarea className="form-control" />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="col-2 col-form-label">Tasks:</label>
+                                <div className="col-10">
+                                    <ul className="pl-3">
+                                        <li>Task 1</li>
+                                        <li>Task 2</li>
+                                    </ul>
+                                    <input className="task-input-field"
+                                           placeholder="type and enter to add new task"
+                                    />
                                 </div>
                             </div>
                         </CardBody>
+                        <CardFooter>
+                            <div className="pull-right">
+                                <input type="submit"
+                                       value="Create"
+                                       className="btn btn-success"
+                                />
+                            </div>
+                        </CardFooter>
                     </Card>
                 </form>
             </Modal>
