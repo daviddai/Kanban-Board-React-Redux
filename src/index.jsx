1ea232687a59b750bf13061e8980741305ebc32c
ddai <ddai@tripadvisor.com>
2019-01-07 16:46:53 +1100

    implement redux wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {Provider} from "react-redux";
+import store from "../../store/index";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import App from "./components/app/App";
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("app")
 );
