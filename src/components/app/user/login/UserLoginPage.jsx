b609ab67e43b55586ef4a17da8c239d43eea6dfe
ddai <ddai@tripadvisor.com>
2019-01-23 14:31:19 +1100

    created user login page prototype

---
+++ src/components/app/user/login/UserLoginPage.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class UserLoginPage extends Component {
+
+    render() {
+        return (
+            <div className="text-center font-weight-bold pt-3">
+                User Login Page WIP
+            </div>
+        );
+    }
+
+}
+
+export default UserLoginPage;
