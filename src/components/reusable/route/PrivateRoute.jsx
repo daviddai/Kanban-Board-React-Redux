5b9c188edaa19b6ab6df243ac65e98c296147b2d
ddai <ddai@tripadvisor.com>
2019-01-25 16:41:37 +1100

    handle user login response wip

--- src/components/reusable/route/PrivateRoute.jsx
+++ src/components/reusable/route/PrivateRoute.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Redirect, Route} from 'react-router-dom';
+import React from "react";
+import {Redirect, Route} from "react-router-dom";
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route {...rest} render={ props => (
